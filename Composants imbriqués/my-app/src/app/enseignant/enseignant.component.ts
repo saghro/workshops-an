a48134caef628ed4ce49f944f26044bbb0a9f305
saghro <ayoubsaghro27@gmail.com>
2023-11-06 01:53:32 +0100

    f

--- Composants imbriqués/my-app/src/app/enseignant/enseignant.component.ts
+++ Composants imbriqués/my-app/src/app/enseignant/enseignant.component.ts
@@ -8,10 +8,19 @@ import { Component, OnInit } from '@angular/core';
 export class EnseignantComponent implements OnInit {
      question : string = '';
      reponse : string = '';
+     messge : string = '';
      constructor(){}
      ngOnInit(): void {
          this.question='quel est le language de programmation utilise pour dev un application angular';
          this.reponse = 'typescript';
      }
-   
+     correction(reponseE : string){
+      if(this.reponse === reponseE){
+        this.messge = 'Bravo'
+      }
+      else{
+        this.messge = 'Echec'
+      }
+         
+     }
 }
